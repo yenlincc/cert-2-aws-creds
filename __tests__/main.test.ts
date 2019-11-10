7401a4e30802bb24c38828576f23d5382fec4184
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2019-11-09 21:20:54 -0800

    Added 2 new tests and realized that role alias has to be created via custom resource

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,27 +1,33 @@
-import {wait} from '../src/wait'
+import {extractValue} from '../src/inputValidation'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-test('throws invalid number', async() => {
-    const input = parseInt('foo', 10);
-    await expect(wait(input)).rejects.toThrow('milleseconds not a number');
+test('extract defined value', async() => {
+    let ok = 'ok';
+    expect(extractValue(ok)).toBe(ok);
 });
 
-test('wait 500 ms', async() => {
-    const start = new Date();
-    await wait(500);
-    const end = new Date();
-    var delta = Math.abs(end.getTime() - start.getTime());
-    expect(delta).toBeGreaterThan(450);
+test('extract undefined value', async() => {
+    var und;
+    expect(() => {extractValue(und)}).toThrow(
+        "failed to extract value because it's undefined");
 });
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-    process.env['INPUT_MILLISECONDS'] = '500';
-    const ip = path.join(__dirname, '..', 'lib', 'main.js');
-    const options: cp.ExecSyncOptions = {
-        env: process.env
-    };
-    console.log(cp.execSync(`node ${ip}`, options).toString());
-});
+// test('wait 500 ms', async() => {
+//     const start = new Date();
+//     await wait(500);
+//     const end = new Date();
+//     var delta = Math.abs(end.getTime() - start.getTime());
+//     expect(delta).toBeGreaterThan(450);
+// });
+
+// // shows how the runner will run a javascript action with env / stdout protocol
+// test('test runs', () => {
+//     process.env['INPUT_MILLISECONDS'] = '500';
+//     const ip = path.join(__dirname, '..', 'lib', 'main.js');
+//     const options: cp.ExecSyncOptions = {
+//         env: process.env
+//     };
+//     console.log(cp.execSync(`node ${ip}`, options).toString());
+// });
